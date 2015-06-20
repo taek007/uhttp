d351e0a93c415d284d07f3e71a467f88d02c2097
hackeris <hackeris@qq.com>
2015-06-20 19:52:09 +0800

    hackeris - move the code of server from main.c into server.c and server.j

---
+++ server.c
@@ -0,0 +1,90 @@
+//
+// Created by hackeris on 15-6-20.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "server.h"
+
+
+void handle_http_request(http_request* request){
+
+    char response[] = "HTTP/1.1 200 OK"CRLF
+    "Server: NHTTP"CRLF
+    "Content-Type: text/html;"CRLF CRLF
+    "<h1>It works!</h1>";
+
+    send(request->socket, response, strlen(response), 0);
+
+    free_http_request(request);
+}
+
+
+int create_server_socket(unsigned short port){
+
+    int server_socket;
+    struct sockaddr_in server_address;
+    int opt = 1;
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("creante socket error %s(errno: %d)\n", strerror(errno), errno);
+        exit(1);
+    }
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(port);
+
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if (bind(server_socket, (struct sockaddr *) &server_address,
+             sizeof(server_address)) == -1) {
+        printf("bind socket error: %s(errno: %d)\n", strerror(errno), errno);
+        exit(2);
+    }
+    return server_socket;
+}
+
+
+void handle_coming_socket(void *_sock){
+
+    int client_socket = *(int*)&_sock;
+
+    http_request request;
+    wrap_http_request(client_socket, &request);
+    handle_http_request(&request);
+}
+
+
+void start_http_server(){
+
+    int server_socket = create_server_socket(8000);
+    int client_socket;
+
+    if (listen(server_socket, 10) == -1) {
+        printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
+        exit(3);
+    }
+
+    printf("waiting for clients.\n");
+    while (1) {
+        //	wait for client's connection.
+        if ((client_socket = accept(server_socket, (struct sockaddr *) NULL,
+                                    NULL)) == -1) {
+            //	if failed
+            printf("accept socket error: %s(errno: %d)", strerror(errno),
+                   errno);
+            break;
+        }
+
+        handle_coming_socket(client_socket);
+    }
+
+    close(server_socket);
+}
---
+++ server.h
@@ -0,0 +1,18 @@
+//
+// Created by hackeris on 15-6-20.
+//
+
+#ifndef UHTTP_SERVER_H
+#define UHTTP_SERVER_H
+
+#include "http.h"
+
+void handle_http_request(http_request* request);
+
+int create_server_socket(unsigned short port);
+
+void handle_coming_socket(void *_sock);
+
+void start_http_server();
+
+#endif //UHTTP_SERVER_H
